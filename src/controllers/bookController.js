a251b2ff744acb95efb3b9e1041bf27a8aba73bd
ruksar <ruksarrasheeda786@gmail.com>
2023-01-23 12:59:07 +0530

    createbook

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,2 +1,13 @@
-const express=require('express')
 
+const bookModel=require("../models/bookModel")
+const userModel=require("../models/userModel")
+
+createBooks= async (req,res)=>{
+    let data=req.body;
+    let userId=req.body.userId;
+    const checkUserId= await userModel.findOne({userId:userId})
+    if(!checkUserId) return res.status(400).send("User does not exist")
+    let createBook= await bookModel.create(data)
+    res.status(201).send({status:true, message:"Success", data:createBook})
+
+}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const bookController=require("../controllers/bookController")
 
 // router.post("/createUser", userController.createUser);
+
+
+router.post("/books", bookController.createBooks)
 
 module.exports = router;
