f4d3282493c9dca28dc7380e3b1f4c705013657a
Mohd Arshad <mohdarshad8668@gmail.com>
2023-01-23 15:55:01 +0530

    validations on user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,27 +6,27 @@ try{
 const userData = req.body
 
 if(!userData.title) return res.status(400).send({status:false,message:"tittle is mandetory"})
-if(typeof(userData.title!=="string")) return res.status(400).send({status:false,message:"wrong format of title"})
-if(!(["Mr", "Mrs", "Miss"].includes(userData.title))) return res.status(400).send({status:false,message:"title can only contain Mr,Mrs, Miss"})
+if(typeof(userData.title) !== 'string') return res.status(400).send({status:false, message:"wrong format of title"})
+if(!(["Mr", "Mrs", "Miss"].includes(userData.title))) return res.status(400).send({status:false, message:"title can only contain Mr,Mrs, Miss"})
 
 if(!userData.name) return res.status(400).send({status:false,message:"name is mandetory"})
 //if(typeof(userData.name!=="string")) return res.status(400).send({status:false,message:"wrong format of name"})
-if(!validation.validate(userData.name)) return res.status(400).send({})
+if(!validation.validate(userData.name)) return res.status(400).send({status:false,message:"wrong format of name"})
 
 if(!userData.phone) return res.status(400).send({status:false,message:"phone is mandetory"})
-if(typeof(userData.phone!=="string")) return res.status(400).send({status:false,message:"wrong format of phone"})
-if(!(userData.phone).match(/((\+)((0[ -]+)|(91 ))(\d{12}+|\d{10}+))|\d{5}([- ])\d{6}/)) return res.status(400).send({status:false,message:"invalid phone number"})
+// if(typeof(userData.phone)!=="string") return res.status(400).send({status:false,message:"wrong format of phone"})
+if(!(userData.phone).match(/^[6-9][0-9]{9}$/)) return res.status(400).send({status:false,message:"invalid phone number"})
 
 if(!userData.email) return res.status(400).send({status:false,message:"email is mandetory"})
-if(typeof(userData.email!=="string")) return res.status(400).send({status:false,message:"wrong format of email"})
+// if(typeof(userData.email!=="string")) return res.status(400).send({status:false,message:"wrong format of email"})
 if(!((userData.email).match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/))) return res.status(400).send({status:false,message:"invalid email address"})
 
 if(!userData.password) return res.status(400).send({status:false,message:"password is mandetory"})
-if(typeof(userData.password!=="string")) return res.status(400).send({status:false,message:"wrong format of password"})
-if(!((userData.password).match(/^.(?=.{8,})(?=.[a-zA-Z])(?=.\d)./))) return res.status(400).send({status:false,message:"length of password should be 8 to 15 characters"})
+// if(typeof(userData.password!=="string")) return res.status(400).send({status:false,message:"wrong format of password"})
+if(!((userData.password).match(/^[a-zA-Z0-9!@#$%^&*]{8,15}$/))) return res.status(400).send({status:false,message:"length of password should be 8 to 15 characters"})
 
 if(userData.address){
-    if(typeof(userData.address)!==Object) return res.status(400).send({status:false,message:"wrong address format"})
+    if(typeof(userData.address)!=='object') return res.status(400).send({status:false,message:"wrong address format"})
     if(userData.address.street){
 
         if(typeof(userData.address.street)!="string") return res.status(400).send({status:false,message:"wrong street format"})
@@ -40,11 +40,12 @@ if(userData.address){
 }
 
 const unique = await userModel.findOne({$or:[{email:userData.email},{phone:userData.phone}]})
-if(unique.email===userData.email) return res.status(400).send({status:false,message:"email already in use"})
-if(unique.phone===userData.phone) return res.status(400).send({status:false,message:"phone already in use"})
-
+if (unique) {
+    if(unique.email==userData.email) return res.status(400).send({status:false,message:"email already in use"})
+    if(unique.phone==userData.phone) return res.status(400).send({status:false,message:"phone already in use"})
+}
 const createdUser = await userModel.create(userData);
-res.status(201).send({status:true,message:success, data:createdUser})
+res.status(201).send({status:true,message:"success", data:createdUser})
 
  }catch(error){
     res.status(500).send({status:false,message:error.message})
