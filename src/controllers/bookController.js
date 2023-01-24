b77ee156eacdae6c0cc8eb40feb0e86bc1a07d33
Mohd Arshad <mohdarshad8668@gmail.com>
2023-01-24 15:37:55 +0530

    update api done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -86,6 +86,54 @@ const getBooks=async(req, res)=>{
     return res.status(200).send({status:true, data:allBooks})
 }
 
+const updateBooks=async(req, res)=>{
 
+    let bookId = req.params.bookId
+    
+    if (!bookId) {
+        return res.status(400).send({status:false, msg:"please send valid params"})
+    }
+
+    if (!validId.isValid(bookId)) {
+        return res.status(400).send({status:false, msg:"please send valid id"})
+    }
+
+    let {title, excerpt, releasedAt, ISBN} = req.body
+
+    if (title) {
+        title = title.trim().toLowerCase();
+
+    if(title=="") return res.status(400).send({status:false,message:"Please input new title to update"})
+    if(!validation.validateTitle(title)) return res.status(400).send({status:false, message:"Please enter valid title"})
+    }
+
+    if (excerpt) {
+        excerpt = excerpt.trim()
+
+        if(excerpt == "") return res.status(400).send({status:false, message:"Please input new excerpt to update "})
+    if(!validation.validateTitle(excerpt)) return res.status(400).send({status:false, message:"Please enter valid excerpt"})
+    }
+
+    if(!releasedAt) return res.status(400).send({status:false, message:"releasedAt is mandatory"})
+    if(typeof(releasedAt) != "string") return res.status(400).send({status:false, message:"Invalid releasedAt format"})
+    if(moment(releasedAt).format("YYYY-MM-DD") != releasedAt) return res.status(400).send({status:false, message:"Invalid date format"})
+
+    //we can do check unique or deleted together
+    let checkUnique = await bookModel.findOne({$or:[{title:title}, {ISBN:ISBN}]})
+
+    if (checkUnique) {
+        return res.status(400).send({status:false, msg:"Duplicate title or ISBN"})
+    }
+
+    //or we can check it here also
+    let updateBook = await bookModel.findOneAndUpdate({_id:bookId, isDeleted:false}, {$set:{title:title, excerpt:excerpt, releasedAt:releasedAt, ISBN:ISBN}}, {new:true});
+
+    if (!updateBook) {
+        return res.status(404).send({status:false, msg:"The book does not exist"})
+    }
+
+    return res.status(200).send({status:true, data:updateBook})
+
+}
 
 module.exports={createBooks, getBooks, updateBooks}
